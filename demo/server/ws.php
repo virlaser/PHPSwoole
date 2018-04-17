6503c02adf63aec7f2598770e96d349bc72a19fe
virlaser <liziyan784617756@foxmail.com>
2018-04-18 01:19:50 +0800

    使用异步 task 来应对耗时任务

--- demo/server/ws.php
+++ demo/server/ws.php
@@ -15,9 +15,18 @@ class Ws {
     public function __construct(){
         $this->ws = new swoole_websocket_server("0.0.0.0", 8812);
 
+        $this->ws->set(
+            [
+                'worker_num' => 2,
+                'task_worker_num' => 2,
+            ]
+        );
+
         $this->ws->on("open", [$this, 'onOpen']);
         $this->ws->on("message", [$this, 'onMessage']);
         $this->ws->on("close", [$this, 'onClose']);
+        $this->ws->on("task", [$this, 'onTask']);
+        $this->ws->on("finish", [$this, 'onFinish']);
 
         $this->ws->start();
     }
@@ -31,11 +40,29 @@ class Ws {
 
     public function onMessage($ws, $frame) {
         echo "ser-push-message:{$frame->data}\n";
+        // todo 10s
+        $data = [
+            'task' => 1,
+            'fd' => $frame->fd
+        ];
+        $ws->task($data);
         $ws->push($frame->fd, "server-push:".date("Y-m-d H:i:s"));
     }
 
     public function onClose($ws, $fd) {
         echo "clientid:{$fd}\n";
+    }
+
+    public function onTask($serv, $taskId, $workerId, $data) {
+        print_r($data);
+        // 耗时场景
+        sleep(10);
+        return "on task finish";
+    }
+
+    public function onFinish($serv, $taskId, $data) {
+        echo "taskId:{$taskId}\n";
+        echo "finish-data-success:{$data}\n";
     }
 }
 
